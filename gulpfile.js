b46293a5b4aacc259e2b2acb275d9b204ae58541
Mwale Kalenga <mwale.kalenga@prosites.space>
2022-03-21 19:05:25 +0200

    Add font display swap rule.

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,12 @@ var cssimport = require('gulp-cssimport');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
+var rename = require("gulp-rename");
 
 var styleWatch = 'src/*.css';
 var cssAssetWatch = 'assets/css/style.css';
 var sassWatch = 'sass/**/*.scss';
+var webFonts = 'assets/css/webfonts/all.css';
 
 function style() {
 	var processors = [
@@ -48,12 +50,35 @@ function compile_sass(done){
 		done();
 }
 
+function process_fonts(){
+    var processors = [
+		atImport,
+		mqpacker,
+		cssnano({
+			calc: {precision: 2}
+		})
+	];
+	return gulp.src('assets/css/webfonts/all.css')
+		.pipe( sourcemaps.init() )
+		.pipe(autoprefixer())
+		.pipe(postcss(processors))
+		.pipe(rename({
+            suffix: '.min'
+        }))
+		.pipe( sourcemaps.write('./') )
+		.pipe(gulp.dest(function(file) {
+			return file.base;
+		  }));
+}
+
 function watch_files() {
 	gulp.watch( styleWatch , style );
 	gulp.watch( cssAssetWatch , import_styles );
 	gulp.watch( sassWatch , compile_sass );
+	gulp.watch( webFonts , process_fonts );
 }
 
-gulp.task('default', gulp.series(style, import_styles, compile_sass));
+gulp.task('default', gulp.series(style, import_styles, compile_sass, process_fonts));
 gulp.task( 'sass', compile_sass );
+gulp.task( 'webfonts', process_fonts );
 gulp.task( 'watch', watch_files );
